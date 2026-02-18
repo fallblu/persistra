a99f0f6a7ba79517696067ef7df6f411978f8d5a
James Mallette <jamesmallmw@gmail.com>
2026-02-18 14:11:25 -0500

    Fix missing import

--- src/persistra/ui/widgets/__init__.py
+++ src/persistra/ui/widgets/__init__.py
@@ -1,11 +1,9 @@
 from .viz_panel import *
 from .context_panel import *
-from .canvas import *
 from .node_browser import *
 
 __all__ = [
         "VizPanel",
         "ContextPanel",
-        "ProjectCanvas",
         "NodeBrowser",
         ]
